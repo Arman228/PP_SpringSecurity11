02e3797b22ef267e005e47a74f873a04101df35a
Arman228 <arman.treyder@bk.ru>
2022-11-24 10:14:06 +0300

    1

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
+    @Autowired
     private UserService userService;
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
