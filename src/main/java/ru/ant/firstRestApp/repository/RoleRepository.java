576ba4232766ce82f01a966261171d91eb2d1a90
Arman228 <arman.treyder@bk.ru>
2023-01-10 10:34:56 +0300

    1

--- src/main/java/ru/ant/firstRestApp/repository/RoleRepository.java
+++ src/main/java/ru/ant/firstRestApp/repository/RoleRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.ant.firstRestApp.model.Role;
 
+import javax.persistence.Basic;
 import java.util.Optional;
 
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
     @Override
+
     Optional<Role> findById(Integer id);
 }
